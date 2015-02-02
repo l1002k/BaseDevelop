fe60c6d8358913580061eb48b20497d956f12ef6
leikun <hzleikun@corp.netease.com>
2015-02-02 15:48:29 +0800

    creat project

---
+++ BaseDevelop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaseDevelop
+//
+//  Created by leikun on 15-2-2.
+//  Copyright (c) 2015年 leikun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BaseDevelop/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BaseDevelop
+//
+//  Created by leikun on 15-2-2.
+//  Copyright (c) 2015年 leikun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
