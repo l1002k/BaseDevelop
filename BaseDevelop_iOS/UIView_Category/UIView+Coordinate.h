96bed892cf0f27a39eedbd81145461a3d7556c7f
leikun <hzleikun@corp.netease.com>
2015-02-02 15:56:12 +0800

    add UIView+Coordinate

---
+++ BaseDevelop_iOS/UIView_Category/UIView+Coordinate.h
@@ -0,0 +1,13 @@
+//
+//  UIView+Coordinate.h
+//  BaseDevelop
+//
+//  Created by leikun on 15-2-2.
+//  Copyright (c) 2015年 leikun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Coordinate)
+
+@end
